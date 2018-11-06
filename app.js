67689a85e1bf8e87556d7f7715a91502d30e4697
Ariana <carrasqa1@hawkmail.newpaltz.edu>
2018-11-06 17:22:16 -0500

    COnfigured website with Docker

---
+++ app.js
@@ -0,0 +1,11 @@
+var express = require('express');
+path = require('path');
+server = express();
+
+var port = 80;
+
+server.use(express.static(path.join(_dirname,'/')));
+
+server.listen(port);
+
+console.log("Listening on port " + port);
